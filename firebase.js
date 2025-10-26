daa206f00401d7538f2ef4d460ae8a52529b7e52
feer <fernandagaonar@gmail.com>
2025-10-26 08:50:13 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC_qgUfDCnZlRhs6MnZTpNDPoJMl5hPEGw",
+  authDomain: "cuyecitosbd-49824.firebaseapp.com",
+  projectId: "cuyecitosbd-49824",
+  storageBucket: "cuyecitosbd-49824.firebasestorage.app",
+  messagingSenderId: "612701891221",
+  appId: "1:612701891221:web:2018fc11f6bb77b96f499c"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
